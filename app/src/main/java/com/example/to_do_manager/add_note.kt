3a2ef0a2652c855047d9469e09c58622b5a3915e
Mohammad Arham Imam <imamarham10@gmail.com>
2020-08-15 13:12:46 +0530

    Describing button's function

--- app/src/main/java/com/example/to_do_manager/add_note.kt
+++ app/src/main/java/com/example/to_do_manager/add_note.kt
@@ -9,37 +9,49 @@ import kotlinx.android.synthetic.main.activity_add_note.*
 import java.lang.Exception
 
 class add_note : AppCompatActivity() {
+    val dbTable = "Notes"
+    var id = 0
     override fun onCreate(savedInstanceState: Bundle?) {
-        val dbTable = "Notes"
-        var id = 0
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_add_note)
 
-        var bundle:Bundle = intent.extras!!
-        try
-        {
-            id = bundle.getInt("ID",0)
-            if(id!=0)
-            {
-                etTitle.setText(bundle.getString("Title"))
-                etDes.setText(bundle.getString("Description"))
+        try {
+            var bundle: Bundle = intent.extras!!
+            id = bundle.getInt("ID", 0)
+            if (id != 0) {
+                etTitle.setText(bundle.getString("name"))
+                etDes.setText(bundle.getString("des"))
+
+            }
+        } catch (ex: Exception) { }
+
+
+    }
+
+    fun buAdd(view: View) {
+        var dbManager = DbManager(this)
+
+        var values = ContentValues()
+        values.put("Title", etTitle.text.toString())
+        values.put("Description", etDes.text.toString())
+        if (id == 0) {
+            val ID = dbManager.Insert(values)
+            if (ID > 0) {
+                Toast.makeText(this, " note is added", Toast.LENGTH_LONG).show()
+                finish()
+            } else {
+                Toast.makeText(this, " cannot add note ", Toast.LENGTH_LONG).show()
             }
         }
-        catch (ex:Exception){}
+        else {
+            var selectionArs = arrayOf(id.toString())
+            val ID = dbManager.Update(values, "ID=?", selectionArs)
+            if (ID > 0) {
+                Toast.makeText(this, " Note Updated", Toast.LENGTH_LONG).show()
+                finish()
+            } else {
+                Toast.makeText(this, " cannot update note ", Toast.LENGTH_LONG).show()
+            }
+        }
     }
-    fun  buAdd(view:View){
-        var dbManager=DbManager(this)
-
-        var values= ContentValues()
-        values.put("Title",etTitle.text.toString())
-        values.put("Description",etDes.text.toString())
-
-        val ID = dbManager.Insert(values)
-        if (ID > 0) {
-            Toast.makeText(this, " Note added", Toast.LENGTH_LONG).show()
-            finish()
-        } else {
-            Toast.makeText(this, " cannot add note ", Toast.LENGTH_LONG).show()
-        }
-        }
 }
